2fc556603c077518c77ed5351a3792918a3a7aff
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-02 19:55:46 +0000

    [

---
+++ 1st odoo academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,86 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "{'name': 'Odoo Academy',\n",
+       " 'summary': 'Academy app to manage Training',\n",
+       " 'description': '\\n    Academy Moduel to manage training: \\n    - Courses\\n    - Sessions\\n    - Attendees\\n    ',\n",
+       " 'author': 'Jeremy Muniak',\n",
+       " 'website': 'http://www.odoo.com/',\n",
+       " 'category': 'Training',\n",
+       " 'version': '0.1',\n",
+       " 'depends': ['base'],\n",
+       " 'data': [],\n",
+       " 'demo': []}"
+      ]
+     },
+     "execution_count": 5,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "# A BASIC MANIFEST FILE\n",
+    "\n",
+    "\n",
+    "# _*_ coding: utf-8 _*_\n",
+    "{\n",
+    "    'name': 'Odoo Academy',\n",
+    "    \n",
+    "    'summary': 'Academy app to manage Training',\n",
+    "    \n",
+    "    'description': \"\"\"\n",
+    "    Academy Moduel to manage training: \n",
+    "    - Courses\n",
+    "    - Sessions\n",
+    "    - Attendees\n",
+    "    \"\"\",\n",
+    "    \n",
+    "    'author': 'Jeremy Muniak',\n",
+    "    \n",
+    "    'website': 'http://www.odoo.com/',\n",
+    "    'category': 'Training',\n",
+    "    'version': '0.1',\n",
+    "    \n",
+    "    'depends': ['base'],\n",
+    "    'data': [],\n",
+    "    'demo': [],\n",
+    "}"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.6.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
