98b5633512c0f82e670bcd2fe90d4e124bdbe965
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-02 23:44:31 +0000

    [ADD] 1st Basic module for Mission to Mars.

---
+++ msn_to_mars/__manifest__.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Msn to Mars',
+    'summary': """Mission to Mars""",
+    'description': """
+        Flight plan:
+        - Pilots
+        - Flight schedule
+        - Provisions
+    """,
+    
+    'author': 'Jeremy M',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
