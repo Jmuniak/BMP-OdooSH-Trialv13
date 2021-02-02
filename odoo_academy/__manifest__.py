6f72bce9fccf3a3d50aa94f577195d1e820bdbec
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-02 20:56:59 +0000

    [ADD] Basic module for academy app

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,26 @@
+# _*_ coding: utf-8 _*_
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy to manage Training""",
+    'description': """
+        Academy to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Jeremy M',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
