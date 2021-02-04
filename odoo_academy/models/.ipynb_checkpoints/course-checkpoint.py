01078e4bae0d7a370f7258191a6d9b719479e680
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-04 00:33:30 +0000

    [ADD] Starting to work with fields

--- odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
+++ odoo_academy/models/.ipynb_checkpoints/course-checkpoint.py
@@ -6,3 +6,7 @@ class Course(models.Model):
     # use the name you want the model to be: Course in this case.
     _name = 'academy.course'
     _description = 'Course Info'
+    
+    name = fields.Char(string='Title', required=True)
+    # next we will have a description field so it can be longer
+    description = fields.Text(string='Description')
