38e1951ab5f23dec2f5d8ba4a52e0b12b61104e8
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-05 18:26:58 +0000

    [FIX] found the missing parentheses in course.py

---
+++ odoo_academy/models/course.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+  # use the name you want the model to be: Course in this case.
+    _name = 'academy.course'
+    _description = 'Course Info'
+    
+    name = fields.Char(string='Title', required=True)
+    # next we will have a description field so it can be longer
+    description = fields.Text(string='Description')
+    
+    # selection field. Level field: the level of the current course
+    level = fields.Selection(string='Level',
+                            selection=[('beginner', 'Beginner'),
+                                      ('intermediate', 'Intermediate'),
+                                      ('advanced', 'Advanced')],
+                            copy=False)
+    
+    # using a reserved field namne
+    active = fields.Boolean(string='Active', default=True)
