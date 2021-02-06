92e17bd5ca8d5acb8b7bd85eb3ca23a4361082cd
Jmuniak <Jeremy.e.muniak@gmail.com>
2021-02-06 05:18:27 +0000

    [ADD] a few bugs in here still  but calling it a day

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -2,7 +2,9 @@
 
 {
     'name': 'Odoo Academy',
+    
     'summary': """Academy to manage Training""",
+    
     'description': """
         Academy to manage Training:
         - Courses
@@ -11,16 +13,26 @@
     """,
     
     'author': 'Jeremy M',
+    
     'website': 'https://www.odoo.com',
+    
     'category': 'Training',
     'version': '0.1',
     
     'depends': ['base'],
     
     'data': [
+        # load the security XML file first. Odoo loads the files in order.
+        'security/academy_security.xml',
+        # if we define any GROUPS in the XML file and we will need to load them before the access rights in the CSV
+        # this can cause an error if not loaded in the right order.
+        'secuirty/ir.model.access.csv',
         
+        #'views/course_views.xml',
+        #'views/academy_menuitems.xml',
     ],
+    
     'demo': [
-        
+        'demo/academy_demo.xml',
     ],
 }
